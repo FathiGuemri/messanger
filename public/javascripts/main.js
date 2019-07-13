59bd72f125d273f539c1db0b8e233d21128097f3
fathi guemri <fathiGuemri>
2019-07-13 17:15:30 +0100

    modified style.css

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -4,7 +4,7 @@ $('form').submit(() => {
     let m = $('#m');
     let name = $('#name');
     let date = new Date();
-    soket.emit('chat message', { msg: m.val(), user: name.val() });
+    soket.emit('chat message', { msg: m.val(), user: name.val(), date: date });
 
     m.val('');
     return false;
@@ -13,7 +13,7 @@ $('form').submit(() => {
 
 soket.on('message', (msg, name, date) => {
 
-    $('#msg').append('<div class="msg"><li id="messages" class="msg_cotainer d-block"> <p class="name">' + name + '</p>' +
+    $('#msg').append('<div class="msg"><li id="messages" class="msg_cotainer d-block"> <p class="name"><i class="fas mr-3  fa-user-tie"></i>' + name + '</p>' +
         msg + '<span class="msg_time">' + date.slice(11, 16) + '</span></li><div>');
 
     $('.msg_card_body').scrollTop($('#msg:last-child').height());
